284be933ad1d7f03ae78520ef0b28bd52f1050fc
吉井駿一 <yoshiishunichi@yoshiishunichinoMacBook-Pro.local>
2020-11-23 03:52:42 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+{
+
+  const imgList = new Vue({
+    el:"#imgList",
+    data:{
+      items:[
+        "img/toilet_paper_center.jpg",
+        "img/toilet_paper_center.jpg",
+        "img/toilet_paper_center.jpg",
+        "img/toilet_paper_center.jpg"
+      ]
+    }
+  })
+}
